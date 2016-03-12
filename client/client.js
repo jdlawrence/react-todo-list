eb6551a99470471e7b2173ed4292b9bfc2ea4a60
jdlawrence <jamil.lawrence@gmail.com>
2016-03-12 15:08:22 -0800

    Working demo with webpack

--- client/client.js
+++ client/client.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from '../components/App';
+
+render(
+  // Define the encompassing components
+  // DOM element we want to mount it to
+  <App/>,
+  document.getElementById('app')
+);
+
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,24 @@
-
+module.exports = {
+  devtool: 'inline-source-map',
+  entry: ['./client/client.js'],
+  output: {
+    path: './dist',
+    filename: 'bundle.js',
+    publicPath: '/'
+  },
+/*  resolve: {*/
+    //extensions: ['', '.js', '.jsx']
+  /*},*/
+  module: {
+    loaders: [
+      {
+        test: /\.js$/m,  
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  }
+};
